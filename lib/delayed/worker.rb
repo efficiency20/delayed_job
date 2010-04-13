fe02340d0e22861d4b5634d798609cb49305ff45
Eirik Dentz Sinclair <eirik@imap.cc>
2010-04-12 22:30:07 -0700

    Add name_prefix fix

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -1,7 +1,7 @@
 module Delayed
   class Worker
     @@sleep_delay = 5
-    
+
     cattr_accessor :sleep_delay
 
     cattr_accessor :logger
@@ -9,6 +9,27 @@ module Delayed
       Merb.logger
     elsif defined?(RAILS_DEFAULT_LOGGER)
       RAILS_DEFAULT_LOGGER
+    end
+
+    # name_prefix is ignored if name is set directly
+    attr_accessor :name_prefix
+
+    def job_max_run_time
+      Delayed::Job.max_run_time
+    end
+
+    # Every worker has a unique name which by default is the pid of the process.
+    # There are some advantages to overriding this with something which survives worker retarts:
+    # Workers can safely resume working on tasks which are locked by themselves. The worker will assume that it crashed before.
+    def name
+      return @name unless @name.nil?
+      "#{@name_prefix}host:#{Socket.gethostname} pid:#{Process.pid}" rescue "#{@name_prefix}pid:#{Process.pid}"
+    end
+
+    # Sets the name of the worker.
+    # Setting the name to nil will reset the default worker name
+    def name=(val)
+      @name = val
     end
 
     def initialize(options={})
